28c3ea3a494aa1d273e0b0803c1da3251089f869
Leonardo Maciel Dutra <Leo.macieldutra@hotmail.com>
2018-11-10 12:30:38 -0300

    Mobile : Criação da casse locação com seus atributos e funções

---
+++ mobile/app/src/main/java/unigran/projeto/br/Classes/Locacao.java
@@ -0,0 +1,41 @@
+package unigran.projeto.br.Classes;
+
+public class Locacao {
+    private Integer dataRetirada, dataDevolucao;
+    private Float kmFinal;
+
+    public Float getKmFinal() {
+        return kmFinal;
+    }
+
+    public void setKmFinal(Float kmFinal) {
+        this.kmFinal = kmFinal;
+    }
+
+    public Integer getDataRetirada() {
+        return dataRetirada;
+    }
+
+    public void setDataRetirada(Integer dataRetirada) {
+        this.dataRetirada = dataRetirada;
+    }
+
+    public Integer getDataDevolucao() {
+        return dataDevolucao;
+    }
+
+    public void setDataDevolucao(Integer dataDevolucao) {
+        this.dataDevolucao = dataDevolucao;
+    }
+
+
+    public void encerrarLocacao(Integer data, Float km){
+
+    }
+    public void manterLocacao(){
+
+    }
+    public void selecionarLocacao(){
+
+    }
+}
