8d301e1dbe8bfdb68d2042e347dc29dddea5804e
junes127 <jnhoanderson2@hotmail.com>
2018-11-10 09:11:26 -0300

    Fixação da navbar, edições no CSS

--- web/index.php
+++ web/index.php
@@ -28,18 +28,16 @@
                 <div class="row center">
                     <h5 class="header col s12 light">A modern responsive front-end framework based on Material Design</h5>
                 </div>
-                <div class="row center">
-                    <button class="waves-light btn modal-trigger" data-target="modal1">Get Started</button>
-                </div>
                 <br><br>
 
             </div>
         </div>
+        <!-- Teste-->
+        
 
 
         <div class="container">
             <div class="section">
-
                 <!--   Icon Section   -->
                 <div class="row">
                     <div class="col s12 m4">
@@ -71,41 +69,29 @@
                 </div>
 
             </div>
+            
             <br><br>
         </div>
-
+        <!-- Footer -->
         <footer class="page-footer orange">
             <div class="container">
                 <div class="row">
                     <div class="col l6 s12">
                         <h5 class="white-text">Company Bio</h5>
-                        <p class="grey-text text-lighten-4">We are a team of college students working on this project like it's our full time job. Any amount would help support and continue development on this project and is greatly appreciated.</p>
-
-
+                        <p class="grey-text text-lighten-4">Loren Ipmsum</p>
                     </div>
                     <div class="col l3 s12">
                         <h5 class="white-text">Settings</h5>
                         <ul>
                             <li><a class="white-text" href="#!">Link 1</a></li>
-                            <li><a class="white-text" href="#!">Link 2</a></li>
-                            <li><a class="white-text" href="#!">Link 3</a></li>
-                            <li><a class="white-text" href="#!">Link 4</a></li>
                         </ul>
                     </div>
                     <div class="col l3 s12">
                         <h5 class="white-text">Connect</h5>
                         <ul>
                             <li><a class="white-text" href="#!">Link 1</a></li>
-                            <li><a class="white-text" href="#!">Link 2</a></li>
-                            <li><a class="white-text" href="#!">Link 3</a></li>
-                            <li><a class="white-text" href="#!">Link 4</a></li>
                         </ul>
                     </div>
-                </div>
-            </div>
-            <div class="footer-copyright">
-                <div class="container">
-                    Made by <a class="orange-text text-lighten-3" href="http://materializecss.com">Materialize</a>
                 </div>
             </div>
         </footer>
