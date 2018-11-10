abd609c9d675efe60f9381be86307329947423fb
Leonardo Maciel Dutra <Leo.macieldutra@hotmail.com>
2018-11-10 12:36:10 -0300

    Mobile : Criação da tela de listagem das locações

---
+++ mobile/app/src/main/java/unigran/projeto/br/Listagem/ListarLocacao.java
@@ -0,0 +1,15 @@
+package unigran.projeto.br.Listagem;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import unigran.projeto.br.locaplus.R;
+
+public class ListarLocacao extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_listar_locacao);
+    }
+}
